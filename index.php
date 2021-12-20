b58c0c61a0c571df82eb4db6bf3d49d9d18d6a1d
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-12-20 22:15:25 +0700

    update time

--- index.php
+++ index.php
@@ -264,18 +264,25 @@ include './connection.php';
     }
 
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+        'November', 'December'
+    ];
     var today, day, h, m, s;
 
     function startTime() {
         today = new Date();
         day = days[today.getDay()];
+        dayOfMonth = today.getDate();
+        month = months[today.getMonth()];
+        year = today.getFullYear();
         h = today.getHours();
         m = today.getMinutes();
         s = today.getSeconds();
         // add a zero in front of numbers<10
         m = checkTime(m);
         s = checkTime(s);
-        document.getElementById('time').innerHTML = day + ", " + h + ":" + m + ":" + s;
+        // document.getElementById('time').innerHTML = day + ", " + h + ":" + m + ":" + s;
+        document.getElementById('time').innerHTML = `${day} ${dayOfMonth} ${month}, ${h}:${m}:${s}`;
     }
     startTime()
 
