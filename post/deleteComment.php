06a41e6a703302a92a836d3446ebb1d483d3eea2
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-12-21 19:15:48 +0700

    no html

--- post/deleteComment.php
+++ post/deleteComment.php
@@ -1,81 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+// session
+session_start();
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../favicon.ico">
-    <title>Delete comment</title>
-</head>
+include '../connection.php';
 
-<body>
-    babi
-    <?php
-    // session
-    session_start();
+// check for POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // get comment id
+    $commentID = $_POST['id'];
+    $reason = $_POST['reason'];
 
-    include '../connection.php';
+    // verify that user is the same as the comment's user
+    $sql = "SELECT * FROM comment WHERE id = '" . $commentID . "'";
+    $result = mysqli_query($conn, $sql);
 
-    // check for POST
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // get comment id
-        $commentID = $_POST['id'];
-        $reason = $_POST['reason'];
-
-        // verify that user is the same as the comment's user
-        $sql = "SELECT * FROM comment WHERE id = '" . $commentID . "'";
-        $result = mysqli_query($conn, $sql);
-
-        if (mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_assoc($result);
-            $commentUser = $row['userID'];
-            $postID = $row['postID'];
-        } else {
-            echo '<div class="alert alert-danger" role="alert">
-        Error: Comment not found.
-        </div>';
-        }
-
-        if ($_SESSION['username'] != $commentUser) {
-            // check isset session isAdmin
-            if (!isset($_SESSION['isAdmin'])) {
-                if ($_SESSION['isAdmin'] != 1) {
-                    header("Location: ../403.php");
-                    return;
-                }
-            }
-        }
-
-        // delete comment
-        $sql = "DELETE FROM comment WHERE id = '$commentID'";
-        $result = mysqli_query($conn, $sql);
-
-        // if reason is not empty, then send notification to comment's user
-        if ($reason != '') {
-            // check session admin or not
-            if ($_SESSION['isAdmin'] == 1) {
-                $sql = "INSERT INTO notification (userID, link, type, details) VALUES ('$commentUser', '#', 'Comment Deleted By Admin', '$reason')";
-                $result = mysqli_query($conn, $sql);
-            }
-        }
-
-
-        // check result, if error print error
-        if (!$result) {
-            $error = 'Error: ' . mysqli_error($conn);
-            echo '<div class="alert alert-danger" role="alert">' . $error . '</div>';
-        } else {
-            echo '<div class="alert alert-success" role="alert">Comment deleted! sucessfully!. Will redirect you back to the post in 3 seconds...</div>';
-            // tell will redirect in 3 seconds
-            header("refresh:3; url=./?id=" . $postID);
-        }
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $commentUser = $row['userID'];
+        $postID = $row['postID'];
     } else {
-        header("Location: ../403.php");
+        echo 'Error: Comment not found.';
     }
 
-    ?>
+    if ($_SESSION['username'] != $commentUser) {
+        // check isset session isAdmin
+        if (!isset($_SESSION['isAdmin'])) {
+            if ($_SESSION['isAdmin'] != 1) {
+                header("Location: ../403.php");
+                return;
+            }
+        }
+    }
 
-</body>
+    // delete comment
+    $sql = "DELETE FROM comment WHERE id = '$commentID'";
+    $result = mysqli_query($conn, $sql);
 
-</html>
+    // if reason is not empty, then send notification to comment's user
+    if ($reason != '') {
+        // check session admin or not
+        if ($_SESSION['isAdmin'] == 1) {
+            $sql = "INSERT INTO notification (userID, link, type, details) VALUES ('$commentUser', '#', 'Comment Deleted By Admin', '$reason')";
+            $result = mysqli_query($conn, $sql);
+        }
+    }
+
+    // check result, if error print error
+    if (!$result) {
+        $error = 'Error: ' . mysqli_error($conn);
+        echo $error;
+    } else {
+        // echo sucess
+        echo 'success';
+    }
+} else {
+    header("Location: ../403.php");
+}
--- post/deletePost.php
+++ post/deletePost.php
@@ -1,77 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+// session
+session_start();
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../favicon.ico">
-    <title>Delete Post</title>
-</head>
+include '../connection.php';
 
-<body>
+// check for POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // get post id
+    $postID = $_POST['id'];
+    $reason = $_POST['reason'];
 
-    <?php
-    // session
-    session_start();
+    // verify that user is the same as the post's user
+    $sql = "SELECT * FROM post WHERE id = '" . $postID . "'";
+    $result = mysqli_query($conn, $sql);
 
-    include '../connection.php';
-
-    // check for POST
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // get post id
-        $postID = $_POST['id'];
-        $reason = $_POST['reason'];
-
-        // verify that user is the same as the post's user
-        $sql = "SELECT * FROM post WHERE id = '" . $postID . "'";
-        $result = mysqli_query($conn, $sql);
-
-        if (mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_assoc($result);
-            $postUser = $row['userID'];
-        } else {
-            echo '<div class="alert alert-danger" role="alert">
-        Error: Post not found.
-        </div>';
-        }
-
-        if ($_SESSION['username'] != $postUser) {
-            // check isset session isAdmin
-            if (!isset($_SESSION['isAdmin'])) {
-                if ($_SESSION['isAdmin'] != 1) {
-                    header("Location: ../403.php");
-                    return;
-                }
-            }
-        }
-
-        // delete post
-        $sql = "DELETE FROM post WHERE id = '$postID'";
-        $result = mysqli_query($conn, $sql);
-
-        // if there is reason send notification to post's user about the deletion
-        if ($reason != '') {
-            // check session isAdmin true or not
-            if ($_SESSION['isAdmin'] == 1) {
-                $sql = "INSERT INTO notification (userID, link, type, details) VALUES ('$postUser', '#', 'Post Deleted By Admin', '$reason')";
-                $result = mysqli_query($conn, $sql);
-            }
-        }
-
-        // check result, if error print error
-        if (!$result) {
-            $error = 'Error: ' . mysqli_error($conn);
-            echo '<div class="alert alert-danger" role="alert">' . $error . '</div>';
-        } else {
-            echo '<div class="alert alert-success" role="alert">Post deleted sucessfully!. Will redirect you to home page in 3 seconds...</div>';
-            // tell will redirect in 3 seconds
-            header("refresh:3; url=../");
-        }
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $postUser = $row['userID'];
     } else {
-        header("Location: ../403.php");
+        echo 'Error: Post not found.';
     }
-    ?>
-</body>
 
-</html>
+    if ($_SESSION['username'] != $postUser) {
+        // check isset session isAdmin
+        if (!isset($_SESSION['isAdmin'])) {
+            if ($_SESSION['isAdmin'] != 1) {
+                header("Location: ../403.php");
+                return;
+            }
+        }
+    }
+
+    // delete post
+    $sql = "DELETE FROM post WHERE id = '$postID'";
+    $result = mysqli_query($conn, $sql);
+
+    // if there is reason send notification to post's user about the deletion
+    if ($reason != '') {
+        // check session isAdmin true or not
+        if ($_SESSION['isAdmin'] == 1) {
+            $sql = "INSERT INTO notification (userID, link, type, details) VALUES ('$postUser', '#', 'Post Deleted By Admin', '$reason')";
+            $result = mysqli_query($conn, $sql);
+        }
+    }
+
+    // check result, if error print error
+    if (!$result) {
+        $error = 'Error: ' . mysqli_error($conn);
+        echo $error;
+    } else {
+        echo 'success';
+    }
+} else {
+    header("Location: ../403.php");
+}
